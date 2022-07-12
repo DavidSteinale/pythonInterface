b6af1631de2a8f32fd7f338450dcdbd4644541fe
David Stein <stein.rs@gmail.com>
2022-07-12 15:13:35 -0300

    Css

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel
 
 app = QApplication(sys.argv)
 janela = QWidget()
+botao = QPushButton('Botão 1',janela) #Define um objeto button, com o nome e a janela queira usar
 
-janela.resize(500,400) # Define o tamanho da janela
+janela.resize(300,300) # Define o tamanho da janela
 janela.setWindowTitle('Primeira janela.') # texto que aparece no topo da janela
+botao.setGeometry(100,25,100,25) # colocado antes do show() , é informado a posição do button o SetGeometry
+botao.setStyleSheet('color:black; background:yellow') # style o botão com CSS
+
+#botao.setBackgroundRole()
 janela.show() #mostra a janela
 
 app.exec() # manda executar a aplicação.
