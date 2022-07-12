c933cbe87159eb1a10d7f0065693f0c169765ae4
David Stein <stein.rs@gmail.com>
2022-07-12 14:20:10 -0300

    Interface

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from PyQt6.QtWidgets import QApplication, QWidget
 app = QApplication(sys.argv)
 janela = QWidget()
 
-janela.resize(500,400)
-janela.setWindowTitle('Primeira janela.')
-janela.show()
+janela.resize(500,400) # Define o tamanho da janela
+janela.setWindowTitle('Primeira janela.') # texto que aparece no topo da janela
+janela.show() #mostra a janela
 
-app.exec()
+app.exec() # manda executar a aplicação.
 
